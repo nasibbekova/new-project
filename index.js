5aae17e979aa710c3e2f501f8e84dd75a7861f4f
nasibbekova <alimon=vam.6703@gmail.com>
2022-11-22 15:02:47 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// let answer = confirm (' Вам есть 18 лет ?')
+
+// if(answer === true) {
+//     console.log ('good');
+// } else {
+//     console.log ('bad');
+// }
+
+// let age =prompt('Сколько вам лет?' , 18)
+  
+// if (age >= 18) {
+//     console.log('bad')
+// } else {
+//     console.log('good')
+// }
+
+let name = prompt( 'What is your name ?')
+let age = prompt( 'Are you upper 18?')
+
+if(name[0]=== 'A' && age === true) {
+    console.log('welcome')
+} else {
+    console.log('go home')
+}
+ 
